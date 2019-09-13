5333259afeae6734163f78819d3e3709f2fb8a02
bessono <bessono@bessono>
2019-09-13 15:43:56 +0300

    Добавление формы занесения карт б базу

--- public_html/views/cards/add-file-form.php
+++ public_html/views/cards/add-file-form.php
@@ -1,5 +1,37 @@
 <?php
 
 use yii\helpers\Html;
+use yii\widgets\ActiveForm;
 
-echo Html::encode('add-file-form.php');
+$form = new ActiveForm();
+$form->begin();
+	?>
+	<div class="container">
+		<div class="row">
+			<div class='col-md-6'>
+	<?php
+	echo Html::label('Вставте (впишите) номера карточек');
+	echo Html::textArea('nombers','',['name'=>'nombers','class'=>'form-control','rows'=>'10','cols'=>'20']);
+	echo Html::tag('hr');
+	echo Html::submitButton('Сохранить карты в базе',['class'=>'btn btn-warning']);
+	?>
+			</div>
+			<div class='col-md-6'>
+				<h3>Помощь</h3>
+				<p>Данный интерфейс служит для внесения карточек в базу данных.</p>
+				<p>Как правило издатели карточек передают номера карт 
+				файлом, Ваша задача заключается в том чтобы скопировать номера этих карточек из файла в поле <b>"Вставте (впишите) номера карточек"</b> так чтобы каждый номер наинался с новой строки, как это показанно в примере:<br>
+					<p>
+					ПервыйНомерКарты<br>
+					ВторойНомерКарты<br>
+					ТретийНомерКарты<br>
+					и т.д.
+					<p>
+				<p>Номера естествено должны быть числом (как правило со штих кода)</p>
+				</p>
+			</div>	
+		</div>
+	</div>
+	<?php
+
+$form->end();
