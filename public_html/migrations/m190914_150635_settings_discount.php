eda5d3a275a7a8a38ee5da3cbddf12bee5102f69
bessono <bessono@bessono>
2019-09-14 18:08:14 +0300

    migrate/create settings_discount

---
+++ public_html/migrations/m190914_150635_settings_discount.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190914_150635_settings_discount
+ */
+class m190914_150635_settings_discount extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190914_150635_settings_discount cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190914_150635_settings_discount cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
