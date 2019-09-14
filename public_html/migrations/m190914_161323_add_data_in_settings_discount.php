92b72fcc52e459fa6e7b20112b0ac4e463b1c8ec
bessono <bessono@bessono>
2019-09-14 19:18:11 +0300

    INSERT DATA to settings_discount TABLE

---
+++ public_html/migrations/m190914_161323_add_data_in_settings_discount.php
@@ -0,0 +1,50 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190914_161323_add_data_in_settings_discount
+ */
+class m190914_161323_add_data_in_settings_discount extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->insert('settings_discount',[
+            'first_step'=>0,
+            'first_discount_percent'=>0,
+            'second_step'=>0,
+            'second_discount_percent'=>0,
+            'third_step'=>0,
+            'third_discount_percent'=>0,
+            'bonus_discount_percent'=>0.1,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->delete('settings_discount');
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190914_161323_add_data_in_settings_discount cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
