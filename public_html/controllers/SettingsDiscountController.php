758136f5d68fe02e82a9f83502ca9d188aadf998
bessono <bessono@bessono>
2019-09-15 13:33:41 +0300

    Fixed return data to discount settings form

--- public_html/controllers/SettingsDiscountController.php
+++ public_html/controllers/SettingsDiscountController.php
@@ -10,7 +10,7 @@ class SettingsDiscountController extends \yii\web\Controller
     public function actionEditForm()
     {
     	$model = new SettingsDiscount();
-    	$current_settings = $model->getSettings();
+    	
     	if($model->load(Yii::$app->request->post()) && $model->validate()){
     		if($model->updateData($model)){
     			Yii::$app->session->setFlash('success','Данные сохранены');
@@ -18,6 +18,7 @@ class SettingsDiscountController extends \yii\web\Controller
     			Yii::$app->session->setFlash('error','Ошибка сохранениея, обратитесь к разработчику');
     		}
     	}
+    	$current_settings = $model->getSettings();
     	return $this->render('edit-form',['model'=>$model,'current_settings'=>$current_settings]);
     }
 
